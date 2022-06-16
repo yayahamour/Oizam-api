4abf95c7f9b908c96acc6eaa3a605d870f19f5e5
BuildTools <unconfigured@null.spigotmc.org>
2022-06-16 14:12:17 +0200

    API

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, File, UploadFile
 from tensorflow import keras
 import uvicorn
 import os
 import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+
 
 bird_dex = pd.read_csv("/code/app/dict_liste_oiseaux.csv")
 app = FastAPI()
@@ -13,12 +17,14 @@ model = keras.models.load_model('/code/app/model.h5')
 def home():
     return({"Bienvenue" : "bienvenue"})
 
-@app.get('/predict/')
-async def get_prediction(request : Request):
-    
-    mat = await request.json()
-    mat = eval(mat)
-    predict = model.predict(mat['Image'])
+@app.post('/predict/')
+async def get_prediction(file: UploadFile(...)):
+    img = image.load_img(file, target_size=(299, 299))
+    img_array = image.img_to_array(img)
+    img_batch = np.expand_dims(img_array, axis=0)
+    preprocessed_image = tf.keras.applications.xception.preprocess_input(img_batch)
+
+    predict = model.predict(preprocessed_image)
     predict = predict.argmax() + 1
 
     return({"result" : bird_dex[bird_dex['number'] == predict]["name"].values[0]})
