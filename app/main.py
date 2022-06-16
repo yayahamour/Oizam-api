b3929fea3cf49df23fe70067a26cc7d20fab44d8
BuildTools <unconfigured@null.spigotmc.org>
2022-06-16 11:58:23 +0200

    API

--- app/main.py
+++ app/main.py
@@ -4,16 +4,16 @@ import uvicorn
 import os
 import pandas as pd
 
-bird_dex = pd.read_csv("/code/app/dict_liste_oiseaux.csv")
+bird_dex = pd.read_csv("./app/dict_liste_oiseaux.csv")
 app = FastAPI()
-model = keras.models.load_model('/code/app/model.h5')
+model = keras.models.load_model('./app/model.h5')
 
 
 @app.get('/')
 def home():
     return({"Bienvenue" : "bienvenue"})
 
-@app.post('/predict/')
+@app.get('/predict/')
 async def get_prediction(request : Request):
     
     mat = await request.json()
